95c8e6378f29b1ccd6aaf6d0b9b5b306db4fc07f
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-02-17 10:15:19 -0500

    finished Intarray

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -11,7 +11,11 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -19,7 +23,11 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+        int product = 1;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+        }
+        return product;
     }
 
     /**
@@ -27,6 +35,12 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        int reverseNum = 0;
+        while( val != 0 ){
+            int digit = val % 10;
+            reverseNum = reverseNum * 10 + digit;
+            val /= 10;
+        }
+        return reverseNum;
     }
 }
