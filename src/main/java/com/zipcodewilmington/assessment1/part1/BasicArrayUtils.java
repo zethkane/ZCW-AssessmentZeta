3212d99093233ca5c4809e9de9b1ff0ab7e6fa56
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-02-17 09:21:33 -0500

    finished basicarray

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
@@ -9,7 +9,8 @@ public class BasicArrayUtils {
      * @return the first element in the array
      */
     public static String getFirstElement(String[] stringArray) {
-        return null;
+
+        return stringArray[0];
     }
 
     /**
@@ -17,7 +18,8 @@ public class BasicArrayUtils {
      * @return the second element in the array
      */
     public static String getSecondElement(String[] stringArray) {
-        return null;
+
+        return stringArray[1];
     }
 
     /**
@@ -25,7 +27,8 @@ public class BasicArrayUtils {
      * @return the last element in the array
      */
     public static String getLastElement(String[] stringArray) {
-        return null;
+
+        return stringArray[stringArray.length - 1];
     }
 
     /**
@@ -33,6 +36,7 @@ public class BasicArrayUtils {
      * @return the second to last element in the array
      */
     public static String getSecondToLastElement(String[] stringArray) {
-        return null;
+
+        return stringArray[stringArray.length - 2];
     }
 }
